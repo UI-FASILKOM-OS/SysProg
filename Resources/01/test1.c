6b167fe769431a54dea8257f84613f22eb1021b4
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2019-09-11 11:57:18 +0700

    rmsbase 01

---
+++ Resources/01/test1.c
@@ -0,0 +1,12 @@
+// Wed Sep 11 11:53:28 WIB 2019
+// Filename:   test1.c
+// To compile: gcc -o test1 test1.c
+// To run:     time ./test.c
+
+
+#include <stdio.h>
+
+void main (void) {
+   printf("Hello, this is a test\n");
+}
+
